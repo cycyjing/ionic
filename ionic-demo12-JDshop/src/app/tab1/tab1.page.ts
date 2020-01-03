03a590f98d4a24530672d204ce419717ceafffc8
cycyjing <cycycyjing@gmail.com>
2020-01-04 10:54:44 +1300

    demo12 v4

--- ionic-demo12-JDshop/src/app/tab1/tab1.page.ts
+++ ionic-demo12-JDshop/src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  @ViewChild('homeSlide', { static: false }) homeSlide;
 
-  constructor() {}
+  slideList: any[] = [];
+  slideOpts = {
+    autoplay: {
+      delay: 2000,
+    },
+    delay: 1000,
+    loop: true
+  };
+  hotList: any[] = [];
+  hotListWidth: any = 111;
 
+  constructor() {
+    for (let i = 1; i < 4; i++) {
+      this.slideList.push({
+        src: 'assets/slide0' + i + '.png',
+        url: ''
+      });
+    }
+    for (let i = 1; i < 10; i++) {
+      this.hotList.push({
+        src: 'assets/0' + i + '.jpg',
+        title: 'item-' + i
+      });
+    }
+    // calculate hotList width
+    this.hotListWidth = this.hotListWidth * this.hotList.length + 'px';
+  }
+
+  slideTouchEnd() {
+    this.homeSlide.startAutoplay();
+  }
 }
