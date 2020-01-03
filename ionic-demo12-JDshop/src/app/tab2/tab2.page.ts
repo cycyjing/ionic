380d3228977950b8d2930090ca73f55a9f1dd21c
cycyjing <cycycyjing@gmail.com>
2020-01-04 12:08:08 +1300

    demo12 v7

--- ionic-demo12-JDshop/src/app/tab2/tab2.page.ts
+++ ionic-demo12-JDshop/src/app/tab2/tab2.page.ts
@@ -6,7 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  leftList: any[] = [];
+  rightList: any[] = [];
 
-  constructor() {}
+  constructor() {
+    for (let i = 1; i <= 20; i++) {
+      this.leftList.push(`item-${i}`);
+    }
+    for (let i = 1; i <= 12; i++) {
+      this.rightList.push({
+        src: 'assets/list' + i + '.jpg',
+        title: 'rightList-' + i
+      });
+    }
+  }
 
 }
