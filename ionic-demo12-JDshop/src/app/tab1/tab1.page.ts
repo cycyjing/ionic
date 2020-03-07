93d9877956149b9a8dd61f748041f48e5bf4b89e
cycyjing <cycycyjing@gmail.com>
2020-03-08 12:15:59 +1300

    demo12 tab1 slides finished

--- ionic-demo12-JDshop/src/app/tab1/tab1.page.ts
+++ ionic-demo12-JDshop/src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -6,13 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  listSlides: any[] = [];
+  @ViewChild('slides', { static: false }) slides;
+  slidesList: any[] = [];
+  slidesOpts = {
+    speed: 2000,
+    autoplay: {
+      delay: 2000,
+    },
+    loop: true,
+  };
 
   constructor() {
-    for (let i = 1; i <= 3; i++) {
-      this.listSlides.push('assets/slide0' + i + '.png');
+    for (var i = 1; i <= 3; i++) {
+      this.slidesList.push({
+        img: 'assets/slide0' + i + '.png',
+        url: ''
+      });
     }
   }
 
-
+  slideTouchEnd() {
+    this.slides.startAutoplay();
+  }
 }
