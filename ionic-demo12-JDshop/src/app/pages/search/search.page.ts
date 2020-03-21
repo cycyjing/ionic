54e0f2f51cb53d816043874b787952d306d2a319
cycyjing <cycycyjing@gmail.com>
2020-03-21 16:57:25 +1300

    demo12 search page product list completed

--- ionic-demo12-JDshop/src/app/pages/search/search.page.ts
+++ ionic-demo12-JDshop/src/app/pages/search/search.page.ts
@@ -10,8 +10,16 @@ import { SearchList } from '../../config/search-list';
 export class SearchPage {
   flag: boolean = false;
   SearchList = SearchList;
+  productList: any[] = [];
 
-  constructor(public navController: NavController) { }
+  constructor(public navController: NavController) {
+    for (let i = 1; i <= 12; i++) {
+      this.productList.push({
+        img: 'assets/list' + i + '.jpg',
+        title: 'item- dfgdfgsdfg dfgdfgsdfgsd  sdfsdfsdf sdfsfsdfs dfse fgd' + i
+      });
+    }
+  }
 
   goBack() {
     this.navController.back();
