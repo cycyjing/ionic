8c641d7c0315e4b5bf18b34f14005aff2a94c152
cycyjing <cycycyjing@gmail.com>
2020-03-26 12:04:32 +1300

    demo12 tab2 change category data from api

--- ionic-demo12-JDshop/src/app/tab2/tab2.page.ts
+++ ionic-demo12-JDshop/src/app/tab2/tab2.page.ts
@@ -1,28 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonService } from '../services/common.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-  navbarList: any[] = [];
-  productList: any[] = [];
+export class Tab2Page implements OnInit {
+  config: any = {};
+  categoryList: any[] = [];
+  selectedid = '';
+  subCategoryList: any[] = [];
 
-  constructor() {
-    // left
-    for (let i = 0; i < 30; i++) {
-      this.navbarList.push('item-' + (i + 1));
-    }
-    // right
-    for (let i = 1; i <= 12; i++) {
-      this.productList.push({
-        img: 'assets/list' + i + '.jpg',
-        url: '',
-        title: 'item' + i
-      });
-    }
+  constructor(public commonService: CommonService) {
+    this.config = commonService.config;
   }
 
+  ngOnInit(): void {
+    // left: navbar
+    this.getNavbarData();
+  }
 
+  getNavbarData() {
+    this.commonService.ajaxGet('api/pcate').then((data: any) => {
+      this.categoryList = data.result;
+      this.getProductsData(this.categoryList[0]._id);
+    });
+  }
+
+  getProductsData(pid) {
+    this.selectedid = pid;
+    this.commonService.ajaxGet('api/pcate?pid=' + pid).then((data: any) => {
+      this.subCategoryList = data.result;
+    });
+  }
 }
