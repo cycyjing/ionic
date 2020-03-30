ea89f642f40c5a4b9ebf44328baf8924cf09a2dc
cycyjing <cycycyjing@gmail.com>
2020-03-30 21:43:56 +1300

    demo12 edit css

--- ionic-demo12-JDshop/src/app/services/storage.service.ts
+++ ionic-demo12-JDshop/src/app/services/storage.service.ts
@@ -6,4 +6,16 @@ import { Injectable } from '@angular/core';
 export class StorageService {
 
   constructor() { }
+
+  set(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  get(key) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+  remove(key) {
+    localStorage.removeItem(key);
+  }
 }
