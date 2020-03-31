c8064b123bcdaf8dfdcd5290bc99c6d4f6b4c2b2
cycyjing <cycycyjing@gmail.com>
2020-04-01 12:57:23 +1300

    demo12 product-content data get from api

--- ionic-demo12-JDshop/src/app/pages/product-content/product-content.page.ts
+++ ionic-demo12-JDshop/src/app/pages/product-content/product-content.page.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonService } from '../../services';
 
 @Component({
   selector: 'app-product-content',
   templateUrl: './product-content.page.html',
   styleUrls: ['./product-content.page.scss'],
 })
-export class ProductContentPage {
+export class ProductContentPage implements OnInit {
   segment: string = 'product';
+  config: any = {};
+  result: any = {};
 
-  constructor() { }
+  constructor(public commonService: CommonService, public activatedRoute: ActivatedRoute) {
+    this.config = commonService.config;
 
+  }
 
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((data) => {
+      this.getProductContentData(data.id);
+    });
+  }
+
+  getProductContentData(id) {
+    this.commonService.ajaxGet('api/pcontent?id=' + id).then((data: any) => {
+      this.result = data.result;
+      console.log(data.result.price);
+    });
+  }
 }
