787c94e28d2b8aea106bfea9f15eebd876e4427e
cycyjing <cycycyjing@gmail.com>
2020-04-21 21:09:00 +1200

    demo12 update address page to get data from api

--- ionic-demo12-JDshop/src/app/pages/address/address.page.ts
+++ ionic-demo12-JDshop/src/app/pages/address/address.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { CommonService, StorageService } from '../../services';
 
 @Component({
   selector: 'app-address',
@@ -7,11 +8,38 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./address.page.scss'],
 })
 export class AddressPage {
+  userinfo: any = {};
+  addressList: any[] = [];
 
-  constructor(public navController: NavController) { }
+  constructor(
+    public navController: NavController,
+    public commonService: CommonService,
+    public storageService: StorageService) {
+    const userinfo = storageService.get('userinfo');
+    if (userinfo && userinfo.username) {
+      this.userinfo = userinfo;
+    } else {
+      navController.navigateForward('/login');
+    }
+  }
+
+  ionViewDidEnter() {
+    this.getAddressesData();
+  }
 
   goBack() {
     this.navController.back();
   }
-  
+
+  getAddressesData() {
+    const sign = this.commonService.createSign({
+      uid: this.userinfo._id,
+      salt: this.userinfo.salt
+    });
+    this.commonService.ajaxGet('api/addressList?uid=' + this.userinfo._id + '&sign=' + sign).then((data: any) => {
+      console.log(data.result);
+      this.addressList = data.result;
+    });
+  }
+
 }
